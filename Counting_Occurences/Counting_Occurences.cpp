94da1d51f4a5e2ef12d4310b36915bf588f8296d
MikiEEE <35975462+MikiEEE@users.noreply.github.com>
2018-02-08 22:41:16 -0600

    Added Header

--- Counting_Occurences/Counting_Occurences.cpp
+++ Counting_Occurences/Counting_Occurences.cpp
@@ -1,32 +1,22 @@
+
 /*
-Reminder: Output Needs to be sorted
- */ 
-
-#include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <random>
-using namespace std;
-
-
-bool findInSet(vector<int> *numbers, int &i);
-void countOccurences(vector<int> *numbers, vector<int> *count,vector<int> *set, const int &i);
-void printElements(vector<int> *numbers, vector<int> *count);
-void generateSet(vector<int> *set, int &numberOfElements);
+Reminder: "set" Needs to be sorted
+*/
+#include "main.h"
 
 int main(int argc, const char * argv[]) {
     vector<int> *set = new vector<int>(0);
     vector<int> *numbers = new vector<int>(0);
     vector<int> *count = new vector<int>(0);
     bool flag(false);
-    int Elements = 100;
+    int Elements = 10000;
+    int index = 0;
     // default set
-    // *set =  {1,1,2,2,2,4,5,5,7,7,7};
+   // *set =  {1,1,2,2,2,4,5,5,7,7,7};
     generateSet(set, Elements);
     for(unsigned int i = 0; i < set->size(); i++) {
-        if(findInSet(numbers, set->at(i)) || flag) {
-            countOccurences(numbers, count, set, i);
+        if(findInSet(numbers, set->at(i), index) || flag) {
+            countOccurences(count, index);
             //cout << numbers->at(numbers->size()-1)<< endl;
             flag = false;
         }
@@ -48,26 +38,6 @@ int main(int argc, const char * argv[]) {
 
 
 
-void generateSet(vector<int> *set, int &numberOfElements) {
-    uniform_int_distribution<int> dice(1,15);
-    uniform_int_distribution<int> smallDice(1,9);
-    mt19937 random;
-    random.seed(time(0));
-    
-    set->push_back(dice(random) % smallDice(random));
-    for(int i = 0; i < numberOfElements - 1; i++) {
-        if(set->back() == dice(random) % smallDice(random)) {
-            set->push_back(dice(random));
-        }
-       else if(set->back() < dice(random) % smallDice(random)) {
-            set->push_back(set->back());
-        }
-       else {
-           set->push_back(dice(random) % smallDice(random));
-       }
-    }
-    return;
-}
 
 void printElements(vector<int> *numbers, vector<int> *count) {
     for(int d = 0; d < numbers->size(); d++) {
@@ -76,20 +46,39 @@ void printElements(vector<int> *numbers, vector<int> *count) {
     return;
 }
 
-void countOccurences(vector<int> *numbers, vector<int> *count, vector<int> *set, const int &i) {
-    for(int d = 0; d < numbers->size(); d++) {
-        if(numbers->at(d) == set->at(i)) {
-            count->at(d)++ ;
-        }
-    }
+void countOccurences(vector<int> *count, const int &index) {
+            count->at(index)++ ;
     return;
 }
 
-bool findInSet(vector<int> *numbers, int &i) {
+bool findInSet(vector<int> *numbers, int &i, int &index) {
     for(unsigned int d = 0; d < numbers->size(); d++){
         if(numbers->at(d) == i) {
+            index = d;
             return true;
         }
     }
     return false;
 }
+
+void generateSet(vector<int> *set, int &numberOfElements) {
+    uniform_int_distribution<int> dice(1,15);
+    uniform_int_distribution<int> smallDice(1,9);
+    mt19937 random;
+    random.seed(time(0));
+    
+    set->push_back(dice(random) % smallDice(random));
+    for(unsigned int i = 0; i < numberOfElements - 1; i++) {
+        if(set->back() == dice(random) % smallDice(random)) {
+            set->push_back(dice(random));
+        }
+        else if(set->back() < dice(random) % smallDice(random)) {
+            set->push_back(set->back());
+        }
+        else {
+            set->push_back(dice(random) % smallDice(random));
+        }
+    }
+    return;
+}
+
