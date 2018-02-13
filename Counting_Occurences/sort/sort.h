8a5a80d210f29132ec7dfc4f955bc128507dca5a
MikiEEE <mikemp1997@gmail.com>
2018-02-13 00:11:33 -0600

    Building sorting methods

---
+++ Counting_Occurences/sort/sort.h
@@ -0,0 +1,24 @@
+//
+//  Sort.hpp
+//  Assignment_2
+//
+//  Created by MikiEEE on 2/10/18.
+// 
+//
+
+#ifndef Sort_hpp
+#define Sort_hpp
+
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+class Sort {
+    public:
+        void bubbleSort(vector<int> *set);
+        void merge(vector<int> *set);
+    private:
+};
+
+#endif /* Sort_hpp */
+
