b05bb13319e4883212d019c19b7bc99c68ed0570
MikiEEE <mikemp1997@gmail.com>
2018-10-17 15:42:21 -0500

    ADD verilog labs

--- Verilog_Labs/Lab6/BCD.v
+++
@@ -1,3 +0,0 @@
-module BCD_Converter(input [11:0]BCD, output [11:0]Binary);
-assign Binary = (BCD[7:4]<<1) + (BCD[7:4]<<3) +  BCD[3:0] + (BCD[11:8]<<6) + (BCD[11:8]<<5) + (BCD[11:8]<<2);
-endmodule
--- Verilog_Labs/Lab6/BCD_tb.v
+++
@@ -1,9 +0,0 @@
-
-module BCDConverter_tb();
-reg [11:0]BCD; 
-wire [11:0]Binary;
- BCD_Converter Instance(BCD, Binary);
-initial begin 
-    BCD = 12'h721;
-end 
-endmodule
