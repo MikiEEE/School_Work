8bba3719b34cffa79c070f4956d235fadd8d0ee4
MikiEEE <mikemp1997@gmail.com>
2018-12-30 23:37:27 -0600

    functionalized everything

--- Biology/PythonBioTools/PunnetTron/PunnettSquareCalculator1.1/HelperFunctions.py
+++ Biology/PythonBioTools/PunnetTron/PunnettSquareCalculator1.1/HelperFunctions.py
@@ -1,3 +1,5 @@
+import pdb
+
 def chunk(xs, n):
     '''Split the list, xs, into n chunks'''
     L = len(xs)
@@ -28,11 +30,28 @@ def generateSinglePairs(length,ParentGenome1, ParentGenome2):
     return ResultHolder
 
 def mix(trait1,trait2):
-    childList = list()
     assert len(trait2) == 4
+    if trait1 == None:
+        return trait2
+    childList = list()
     for x in trait1:
-            childList.append(x + trait2[0])
-            childList.append(x + trait2[1])
-            childList.append(x + trait2[2])
-            childList.append(x + trait2[3])
+        childList.append(x + trait2[0])
+        childList.append(x + trait2[1])
+        childList.append(x + trait2[2])
+        childList.append(x + trait2[3])
     return childList
+
+def createTraits(seperateTraits):
+    result = seperateTraits[0]
+    for alleles in seperateTraits[1:]:
+        result = mix(result, alleles)
+    return result
+
+def buildGenome(Traits):
+    Genomes = dict()
+    for y in Traits:
+        if Genomes.get(y, -1) == -1:
+            Genomes[y] = 1
+        else:
+            Genomes[y] += 1
+    return Genomes
--- Biology/PythonBioTools/PunnetTron/PunnettSquareCalculator1.1/PunnettSquareCalculator.py
+++ Biology/PythonBioTools/PunnetTron/PunnettSquareCalculator1.1/PunnettSquareCalculator.py
@@ -1,45 +1,32 @@
 import HelperFunctions as hp
+from functools import reduce
 
-# parent1 = input("Please Input Parent 1's Alleles: ")
-# parent2 = input("Please Input Parent 2's Alleles: ")
-#
-parent1 = "AabbcCDdEeFfHHIiLljjMm"
-parent2 = "aaBbccDDeeFFhhiiLlJjmm"
+def calculateGeneticTraits(parent1, parent2):
+    potentialChild = list()
+    potentialChild = hp.generateSinglePairs(len(parent1), parent1, parent2)
+    potentialChild = [''.join(sorted(x)) for x in potentialChild]
+    seperateTraits = hp.chunk(potentialChild, int(len(parent1)/2))
+    Traits = hp.createTraits(seperateTraits)
+    del seperateTraits
+    Genomes = hp.buildGenome(Traits)
+    del Traits
+    return Genomes
 
-
-# parent1 = "AabbcCDd"
-# parent2 = "aaBbccDd"
-
-potentialChild = list()
-potentialChild = hp.generateSinglePairs(len(parent1), parent1, parent2)
-Genomes = dict()
-potentialChild = [''.join(sorted(x)) for x in potentialChild]
-seperateTraits = hp.chunk(potentialChild, int(len(parent1)/2))
-
-print('generateSinglePairs',potentialChild)
-print('CHUNK',seperateTraits)
-
-Traits = seperateTraits[0]
-
-for x in range(1,int(len(parent1)/2)):
-    Traits= hp.mix(Traits,seperateTraits[x])
-
-del seperateTraits
-
-for y in Traits:
-    if Genomes.get(y, -1) == -1:
-        Genomes[y] = 1
-    else:
-        Genomes[y] += 1
-
-traitLength = len(Traits)
-del Traits
+def printGenomes(Genomes):
+    traitLength = reduce(lambda x,y: x + y, Genomes.values())
+    index = 1
+    for x in Genomes.keys():
+        print(str(index) + ".","  ",str(x),"      ",str((Genomes[x]/traitLength) * 100) + "%" )
+        index+=1
+    del Genomes
+    return
 
 
-print("#   Genome  Percentage")
-index = 1
-for x in Genomes.keys():
-    print(str(index)".","  ",str(x),"      ",str((Genomes[x]/traitLength) * 100) + "%" )
-    index+=1
 
-del Genomes
+if __name__ == '__main__':
+    parent1 = "AabbcCDdEeFfHHIiLljjMm"
+    parent2 = "aaBbccDDeeFFhhiiLlJjmm"
+    parent1 = "AabbcCDd"
+    parent2 = "aaBbccDd"
+    genome = calculateGeneticTraits(parent1, parent2)
+    printGenomes(genome)
