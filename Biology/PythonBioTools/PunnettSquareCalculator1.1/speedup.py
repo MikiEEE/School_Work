40aa339e2a65ad89351f5278ff56f865726f95c1
MikiEEE <mikemp1997@gmail.com>
2019-01-03 23:55:54 -0600

    Removed PunnettTron

--- Biology/PythonBioTools/PunnettSquareCalculator1.1/speedup.py
+++
@@ -1,9 +0,0 @@
-def generateSinglePairs(length,ParentGenome1, ParentGenome2):
-    assert len(ParentGenome1) == len(ParentGenome2)
-    assert length > 0
-    ResultHolder = list()
-    for newAlleleStart in range(0,length,2):
-        for parent1Allele in ParentGenome1[newAlleleStart:newAlleleStart+2]:
-            for parent2Allele in ParentGenome2[newAlleleStart:newAlleleStart+2]:
-                ResultHolder.append(parent1Allele + parent2Allele)
-    return ResultHolder
